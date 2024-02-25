76f1eced8af9da44e4520c3ae3013cfdb1ba15e6
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-02-26 02:47:13 +0900

    マルチプロジェクト化

---
+++ integration-test/src/main/kotlin/org/contourgara/examination2gara/Examination2GaraApplication.kt
@@ -0,0 +1,11 @@
+package org.contourgara.examination2gara
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Examination2GaraApplication
+
+fun main(args: Array<String>) {
+  runApplication<Examination2GaraApplication>(*args)
+}
---
+++ integration-test/src/test/kotlin/org/contourgara/examination2gara/Examination2GaraApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.contourgara.examination2gara
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Examination2GaraApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
