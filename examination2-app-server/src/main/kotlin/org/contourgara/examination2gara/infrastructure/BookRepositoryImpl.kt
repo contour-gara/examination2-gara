e0794e4e47df466d52ef9ed942b4ceb1100aab1e
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-03-04 02:07:13 +0900

    全件検索の Repository と Mapper 結合

--- examination2-app-server/src/main/kotlin/org/contourgara/examination2gara/infrastructure/BookRepositoryImpl.kt
+++ examination2-app-server/src/main/kotlin/org/contourgara/examination2gara/infrastructure/BookRepositoryImpl.kt
@@ -5,11 +5,12 @@ import org.contourgara.examination2gara.domain.BookRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-class BookRepositoryImpl: BookRepository {
+class BookRepositoryImpl(
+  private val bookMapper: BookMapper
+): BookRepository {
   override fun findAll(): List<Book> {
-    return listOf(
-      Book("1", "テスト駆動開発", "Kent Beck", "オーム社", 3080),
-      Book("2", "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860)
-    )
+    return bookMapper.findAll().map {
+      it.toModel()
+    }
   }
 }
--- examination2-app-server/src/test/kotlin/org/contourgara/examination2gara/infrastructure/BookRepositoryImplTest.kt
+++ examination2-app-server/src/test/kotlin/org/contourgara/examination2gara/infrastructure/BookRepositoryImplTest.kt
@@ -5,11 +5,16 @@ import org.contourgara.examination2gara.domain.Book
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
 
 class BookRepositoryImplTest {
   @InjectMocks
   lateinit var sut: BookRepositoryImpl
+
+  @Mock
+  lateinit var bookMapper: BookMapper
 
   @BeforeEach
   fun setUp() {
@@ -18,6 +23,12 @@ class BookRepositoryImplTest {
 
   @Test
   fun `全件検索の場合、本情報のリストを返す`() {
+    // setup
+    doReturn(listOf(
+      BookEntity("1", "テスト駆動開発", "Kent Beck", "オーム社", 3080),
+      BookEntity("2", "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860)
+    )).`when`(bookMapper).findAll()
+
     // execute
     val actual: List<Book> = sut.findAll()
 
