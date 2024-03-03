2a1e95e9fabb179d8887fb54881740748ee23b61
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-03-04 00:54:18 +0900

    全件検索の Repository と UseCase 結合

--- examination2-app-server/src/main/kotlin/org/contourgara/examination2gara/application/FindAllBooksUseCase.kt
+++ examination2-app-server/src/main/kotlin/org/contourgara/examination2gara/application/FindAllBooksUseCase.kt
@@ -1,22 +1,22 @@
 package org.contourgara.examination2gara.application
 
 import org.contourgara.examination2gara.domain.Book
+import org.contourgara.examination2gara.domain.BookRepository
 import org.springframework.stereotype.Service
 
 /**
  * FindAllBooksUseCase は、本情報の全件検索のロジックです。
  */
 @Service
-class FindAllBooksUseCase {
+class FindAllBooksUseCase(
+  private val bookRepository: BookRepository
+) {
   /**
    * 本情報の全件検索を実行します。
    *
    * @return 本情報のリスト
    */
   fun execute(): List<Book> {
-    return listOf(
-      Book("1", "テスト駆動開発", "Kent Beck", "オーム社", 3080),
-      Book("2", "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860)
-    )
+    return bookRepository.findAll()
   }
 }
--- examination2-app-server/src/test/kotlin/org/contourgara/examination2gara/application/FindAllBooksUseCaseTest.kt
+++ examination2-app-server/src/test/kotlin/org/contourgara/examination2gara/application/FindAllBooksUseCaseTest.kt
@@ -2,14 +2,20 @@ package org.contourgara.examination2gara.application
 
 import org.assertj.core.api.Assertions.*
 import org.contourgara.examination2gara.domain.Book
+import org.contourgara.examination2gara.domain.BookRepository
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
 
 class FindAllBooksUseCaseTest {
   @InjectMocks
   lateinit var sut: FindAllBooksUseCase
+
+  @Mock
+  lateinit var bookRepository: BookRepository
 
   @BeforeEach
   fun setup() {
@@ -18,6 +24,12 @@ class FindAllBooksUseCaseTest {
 
   @Test
   fun `実行した場合、すべての本を返す`() {
+    // setup
+    doReturn(listOf(
+      Book("1", "テスト駆動開発", "Kent Beck", "オーム社", 3080),
+      Book("2", "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860)
+    )).`when`(bookRepository).findAll()
+
     // execute
     val actual: List<Book> = sut.execute()
 
