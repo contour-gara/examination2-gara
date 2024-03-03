361cbe655829485e74cdd925fb169b71209b9d9f
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-03-04 02:12:37 +0900

    Ktdoc 追加

--- examination2-app-server/src/main/kotlin/org/contourgara/examination2gara/domain/BookRepository.kt
+++ examination2-app-server/src/main/kotlin/org/contourgara/examination2gara/domain/BookRepository.kt
@@ -1,5 +1,13 @@
 package org.contourgara.examination2gara.domain
 
+/**
+ * BookRepository は、本情報を永続化するためのインターフェースです。
+ */
 interface BookRepository {
+  /**
+   * 本方法を全件取得します。
+   *
+   * @return 本情報のリスト。
+   */
   fun findAll(): List<Book>
 }
--- examination2-app-server/src/main/kotlin/org/contourgara/examination2gara/infrastructure/BookRepositoryImpl.kt
+++ examination2-app-server/src/main/kotlin/org/contourgara/examination2gara/infrastructure/BookRepositoryImpl.kt
@@ -4,6 +4,9 @@ import org.contourgara.examination2gara.domain.Book
 import org.contourgara.examination2gara.domain.BookRepository
 import org.springframework.stereotype.Repository
 
+/**
+ * BookRepositoryImpl は、本情報をデータベースに永続化するクラスです。
+ */
 @Repository
 class BookRepositoryImpl(
   private val bookMapper: BookMapper
